301cee1edf89718ff142b4cb77838a4af7419043
Luisb <luis.brito@siwgroup.com>
2019-10-24 12:31:16 -0300

    Refactored react component using es6 class syntax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var style= {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Helvetica'
+class Message extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 id="heading-element" className="header">Hello World!</h1>
+                <p>React is fun!</p>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-    <div style={style}>
-        <h1 id="heading-element" className="header">Hello World!</h1>
-        <p>React is fun!</p>
-    </div>,
-    document.getElementById('root')
+    <Message />, document.getElementById('root')
 )

b724c060fbbdf0d4bcdebac71d02c31b41a57087
Luisb <luis.brito@siwgroup.com>
2019-10-24 17:45:44 -0300

    Added function and button that toggles state of Library, refactored constructor to make code cleaner

--- src/Library.js
+++ src/Library.js
@@ -12,19 +12,29 @@ const Book = ({title, author, pages}) => {
 
 // To use local state Class component is needed
 class Library extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { // State is just an Object, that has as many keys as one sets up
-            open: true
-        }
-    }
 
+    state = { open: false } // This gets rid of constructor below
+
+    // constructor(props) {
+    //     super(props)
+    //     this.state = { // State is just an Object, that has as many keys as one sets up
+    //         open: true
+    //     }
+    //     this.toggleOpenClosed = this.toggleOpenClosed.bind(this) // Makes 'this' accessible to custom method
+    // }
+
+    toggleOpenClosed = () => { // Arrow functions automatically bind 'this' inside of the context of this function
+        this.setState(prevState => ({ //Returns this Object, uses callback to make it async
+            open: !prevState.open // Change it to the opposite
+        }))
+    }
     render() {
         console.log(this.state)
         const { books } = this.props
         return ( 
             <div>
                 <h2>The library is {this.state.open ? 'open' : 'closed'}</h2> { /*Conditional Rendering using ternary or inline if statement */}
+                <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
                     (book, i) =>
                         <Book

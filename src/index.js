d4091240b4ace174897b3748dd94c419fd6a645e
Luisb <luis.brito@siwgroup.com>
2019-10-24 12:40:05 -0300

    Added components' properties

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ class Message extends React.Component {
     render() {
         return (
             <div>
-                <h1 id="heading-element" className="header">Hello World!</h1>
-                <p>React is fun!</p>
+                {console.log(this.props)}
+                <h1 style={{color: this.props.color}}>
+                    {this.props.msg}
+                </h1>
+                <p>I'll check back in {this.props.minutes} minutes</p>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <Message />, document.getElementById('root')
+    <Message color="grey" msg="how are you?" minutes={5}/>, document.getElementById('root')
 )

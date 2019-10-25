c9b80692dda125a3077d89182580ef35f58f53d0
Luisb <luis.brito@siwgroup.com>
2019-10-25 12:04:51 -0300

    Used component life cycle to fetch data and display it

--- src/Library.js
+++ src/Library.js
@@ -27,8 +27,25 @@ class Library extends React.Component {
     state = { // This gets rid of constructor 
         open: true, 
         freeBookmark: true,
-        hiring: true
-    } 
+        hiring: true,
+        data: [],
+        loading: false
+    }
+
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+            .then(data => data.json()) // Turn data from API into json
+            .then(data => this.setState({data, loading: false})) // Set data to state.date and loading to false
+    }
+
+    componentDidUpdate() {
+        console.log("The component just updated!")
+    }
+
+    componentWillUnmount() {
+        console.log("The component will unmount!")
+    }
 
     toggleOpenClosed = () => { // Arrow functions automatically bind 'this' inside of the context of this function
         this.setState(prevState => ({ //Returns this Object, uses callback to make it async
@@ -36,11 +53,25 @@ class Library extends React.Component {
         }))
     }
     render() {
-        console.log(this.state)
+        // console.log(this.state)
         const { books } = this.props
         return ( 
             <div>
                 {this.state.hiring ? <Hiring /> : <NotHiring />}
+                {this.state.loading
+                    ? "loading..."
+                    : <div>
+                        {this.state.data.map((product, i) => {
+                            return (
+                                <div key={i}>
+                                    <h3>Library Product of the Week!</h3>
+                                    <h4>{product.name}</h4>
+                                    <img src={product.image} height={100} alt={product.name}/>
+                                </div>
+                            )
+                        })}
+                      </div>
+                }
                 <h2>The library is {this.state.open ? 'open' : 'closed'}</h2> { /*Conditional Rendering using ternary or inline if statement */}
                 <button onClick={this.toggleOpenClosed}>Change</button>
                 {books.map(
